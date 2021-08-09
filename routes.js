74c5fe86c170fe0f3f6d3c8611d602011e38172c
Vanessa Rolim de Aquino <rolimvanessa30@gmail.com>
2021-08-09 16:06:59 -0300

    Iniciando o CRUD

--- routes.js
+++ routes.js
@@ -37,20 +37,23 @@ router.get('/about',(req,res)=>{
     res.render('pages/about');
 });
 
-router.post('/cadastro/remove',(req,res)=>{
-    let usuario={name: "", email: ""};
-    /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
+router.get('/cadastro/remove/:id',(req,res)=>{
+    res.send('Remoção realizada com sucesso');
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
 });
 
-router.get('/cadastro',(req,res)=>{
-  
-    res.render('pages/cadastro');
+router.get('/cadastro/update/:id',(req,res)=>{
+    res.send('Atualização realizada com sucesso');
+
+});
+
+router.get('/cadastro',(req,res)=>{ //callback - função que trata determinado evento
+    let users = [
+        {name:"Ana Lúcia Mendes Paiva", endereco: "Rua Dom Jeronimo, 769", email:"luciluci@gmail.com", login:"lucinha40", idade:"18", altura:"1,83cm"},
+        {name:"Marcos Anderson Silva", endereco: "Rua Armindo, 79", email:"marcosilva63@gmail.com",login:"marcos123", idade:"25", altura:"1,65cm"},
+        {name:"Julia Almeida Teixeira", endereco: "Rua 789, 79", email:"texeira20@gmail.com",login:"Juju789", idade:"40", altura:"1,73"}
+    ];
+    res.render('pages/cadastro',{users});
 });
 
 router.get('/cadastro/insert',(req,res)=>{
@@ -59,7 +62,7 @@ router.get('/cadastro/insert',(req,res)=>{
         usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
     } */
 
-    let result = db.inserirDado(usuario);
+    let result = db.inserirDado(usuariusers);
     console.log(result);
     //res.render('pages/insert',{result});
 });
